db9c2920329baab193d9499ea64d9100a91d74f5
Carlton Stith <stith@temple.edu>
2025-05-28 15:59:48 -0400

    remove env variable

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
 // export const PORTFOLIO_API = "http://localhost:3000/api/projects";
-export const PORTFOLIO_API = process.env.PORTFOLIO_API || "https://www.carltonstith.com/api/projects";
+export const PORTFOLIO_API = "https://www.carltonstith.com/api/projects";
