e66a8d15485402351a3b613cf844678742d5d3f8
Carlton Stith <stith@temple.edu>
2025-05-30 14:15:09 -0400

    updated text

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,5 @@
 import { Metadata } from "next";
 import {PortfolioCard} from "../components/PortfolioCard";
-// import {PortfolioCard} from "../portfoliocard/page";
-// import { Suspense } from "react";
 import { PortfolioModal } from "./PortfolioModal";
 import { PORTFOLIO_API } from "@/lib/constants";
 
@@ -30,12 +28,8 @@ export default async function Portfolio({
   searchParams 
 }: {
   searchParams: Promise<{id:string, modal:string}>;
-  //searchParams: Promise<{ modal:string, id:string}>; 
 }) {
-  //const { showModal } = await searchParams; // make showModal = true
   const params = await searchParams;
-  // const showModal = params?.showModal === "true";
-  // const portfolioId = params.portfolioId;
   const showModal = params?.modal === "true";
   const portfolioId = params?.id;
 
@@ -51,20 +45,18 @@ export default async function Portfolio({
     }
     return res;
   });
-  //console.log("response: ", response);
   const projects: Portfolio[] = await response.json();
-  //console.log("projects: ", projects);
+
+  const intro = `Hi. Thanks for taking a look at my portfolio.
+Some of the projects that are featured here are labors of love and others were paid projects.
+I'm currently available for hire.`;
 
   return (
     <section>
       <div className="flex items-center justify-center min-h-screen py-20">
         <div className="max-w-5xl mx-auto px-4">
           <h2 className="text-3xl font-bold mb-8 text-center">
-            {""}
-            {/* Some of my work */}
-            Hi. Thanks for taking a look at my portfolio.
-Some of the projects that are featured here are labors of love and other were paid projects.
-I'm currently available for hire.
+            {intro}
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {projects &&
@@ -74,37 +66,10 @@ I'm currently available for hire.
 
             {showModal && (
               <PortfolioModal id={portfolioId} />
-              // <Suspense key={portfolioId} fallback={<div>Loading...</div>}>
-              //   <PortfolioModal id={portfolioId} />
-              // </Suspense>
             )}
           </div>
         </div>
       </div>
     </section>
   );
-  // return (
-  //   <section>
-  //     <div className="flex items-center justify-center min-h-screen py-20">
-  //       <div className="max-w-5xl mx-auto px-4">
-  //         <h2 className="text-3xl font-bold mb-8 text-center">
-  //           {""}
-  //           Featured Projects
-  //         </h2>
-  //         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-  //           {projects &&
-  //             projects.map((projects: Portfolio, idx: number) => (
-  //               <PortfolioCard key={idx} {...projects} />
-  //             ))}
-
-  //           {showModal && (
-  //             <Suspense key={portfolioId} fallback={<div>Loading...</div>}>
-  //               <PortfolioModal id={portfolioId} />
-  //             </Suspense>
-  //           )}
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </section>
-  // );
 }
